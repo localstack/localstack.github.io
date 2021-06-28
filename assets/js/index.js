841dfad534e7b8dfd1f6df04f73f4031ab087d8c
Marten Sigwart <marten@okamaja.de>
2021-06-28 13:42:26 +0200

    fix intendation

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,131 +22,131 @@ function inputFocus(e) {
 
 if (suggestions != null) {
 
-document.addEventListener('click', function(event) {
+  document.addEventListener('click', function(event) {
 
-  var isClickInsideElement = suggestions.contains(event.target);
+    var isClickInsideElement = suggestions.contains(event.target);
 
-  if (!isClickInsideElement) {
-    suggestions.classList.add('d-none');
-  }
+    if (!isClickInsideElement) {
+      suggestions.classList.add('d-none');
+    }
 
-});
-
-/*
-Source:
-  - https://dev.to/shubhamprakash/trap-focus-using-javascript-6a3
-*/
-
-document.addEventListener('keydown',suggestionFocus);
-
-function suggestionFocus(e){
-
-  const focusableSuggestions= suggestions.querySelectorAll('a');
-  const focusable= [...focusableSuggestions];
-  const index = focusable.indexOf(document.activeElement);
-
-  let nextIndex = 0;
-
-  if (e.keyCode === 38) {
-    e.preventDefault();
-    nextIndex= index > 0 ? index-1 : 0;
-    focusableSuggestions[nextIndex].focus();
-  }
-  else if (e.keyCode === 40) {
-    e.preventDefault();
-    nextIndex= index+1 < focusable.length ? index+1 : index;
-    focusableSuggestions[nextIndex].focus();
-  }
-
-}
-
-/*
-Source:
-  - https://github.com/nextapps-de/flexsearch#index-documents-field-search
-  - https://raw.githack.com/nextapps-de/flexsearch/master/demo/autocomplete.html
-*/
-
-(function(){
-
-  var index = new FlexSearch({
-    preset: 'score',
-    cache: true,
-    doc: {
-        id: 'id',
-        field: [
-          'title',
-          'description',
-          'content',
-        ],
-        store: [
-          'href',
-          'title',
-          'description',
-        ],
-    },
   });
 
-  var docs = [
-    {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
-      {
-        id: {{ $index }},
-        href: "{{ .RelPermalink | relURL }}",
-        title: {{ .Title | jsonify }},
-        description: {{ .Params.description | jsonify }},
-        content: {{ .Content | jsonify }}
-      },
-    {{ end -}}
-  ];
+  /*
+  Source:
+    - https://dev.to/shubhamprakash/trap-focus-using-javascript-6a3
+  */
 
-  index.add(docs);
+  document.addEventListener('keydown',suggestionFocus);
 
-  userinput.addEventListener('input', show_results, true);
-  suggestions.addEventListener('click', accept_suggestion, true);
+  function suggestionFocus(e){
 
-  function show_results(){
+    const focusableSuggestions= suggestions.querySelectorAll('a');
+    const focusable= [...focusableSuggestions];
+    const index = focusable.indexOf(document.activeElement);
 
-    var value = this.value;
-    var results = index.search(value, 5);
-    var entry, childs = suggestions.childNodes;
-    var i = 0, len = results.length;
+    let nextIndex = 0;
 
-    suggestions.classList.remove('d-none');
-
-    results.forEach(function(page) {
-
-      entry = document.createElement('div');
-
-      entry.innerHTML = '<a href><span></span><span></span></a>';
-
-      a = entry.querySelector('a'),
-      t = entry.querySelector('span:first-child'),
-      d = entry.querySelector('span:nth-child(2)');
-
-      a.href = page.href;
-      t.textContent = page.title;
-      d.textContent = page.description;
-
-      suggestions.appendChild(entry);
-
-    });
-
-    while(childs.length > len){
-
-        suggestions.removeChild(childs[i])
+    if (e.keyCode === 38) {
+      e.preventDefault();
+      nextIndex= index > 0 ? index-1 : 0;
+      focusableSuggestions[nextIndex].focus();
+    }
+    else if (e.keyCode === 40) {
+      e.preventDefault();
+      nextIndex= index+1 < focusable.length ? index+1 : index;
+      focusableSuggestions[nextIndex].focus();
     }
 
   }
 
-  function accept_suggestion(){
+  /*
+  Source:
+    - https://github.com/nextapps-de/flexsearch#index-documents-field-search
+    - https://raw.githack.com/nextapps-de/flexsearch/master/demo/autocomplete.html
+  */
 
-      while(suggestions.lastChild){
+  (function(){
 
-          suggestions.removeChild(suggestions.lastChild);
+    var index = new FlexSearch({
+      preset: 'score',
+      cache: true,
+      doc: {
+          id: 'id',
+          field: [
+            'title',
+            'description',
+            'content',
+          ],
+          store: [
+            'href',
+            'title',
+            'description',
+          ],
+      },
+    });
+
+    var docs = [
+      {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
+        {
+          id: {{ $index }},
+          href: "{{ .RelPermalink | relURL }}",
+          title: {{ .Title | jsonify }},
+          description: {{ .Params.description | jsonify }},
+          content: {{ .Content | jsonify }}
+        },
+      {{ end -}}
+    ];
+
+    index.add(docs);
+
+    userinput.addEventListener('input', show_results, true);
+    suggestions.addEventListener('click', accept_suggestion, true);
+
+    function show_results(){
+
+      var value = this.value;
+      var results = index.search(value, 5);
+      var entry, childs = suggestions.childNodes;
+      var i = 0, len = results.length;
+
+      suggestions.classList.remove('d-none');
+
+      results.forEach(function(page) {
+
+        entry = document.createElement('div');
+
+        entry.innerHTML = '<a href><span></span><span></span></a>';
+
+        a = entry.querySelector('a'),
+        t = entry.querySelector('span:first-child'),
+        d = entry.querySelector('span:nth-child(2)');
+
+        a.href = page.href;
+        t.textContent = page.title;
+        d.textContent = page.description;
+
+        suggestions.appendChild(entry);
+
+      });
+
+      while(childs.length > len){
+
+          suggestions.removeChild(childs[i])
       }
 
-      return false;
-  }
+    }
 
-}());
+    function accept_suggestion(){
+
+        while(suggestions.lastChild){
+
+            suggestions.removeChild(suggestions.lastChild);
+        }
+
+        return false;
+    }
+
+  }());
 
 }
