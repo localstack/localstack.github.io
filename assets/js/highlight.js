42cb8f3f2cbce5ab010ee9753cc4c0073a09683e
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-07-25 17:12:30 +0200

    enable syntax highlighting for additional languages

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -2,20 +2,30 @@ import hljs from 'highlight.js/lib/core';
 
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
+import java from 'highlight.js/lib/languages/java';
+import properties from 'highlight.js/lib/languages/properties';
 import bash from 'highlight.js/lib/languages/bash';
 import htmlbars from 'highlight.js/lib/languages/htmlbars';
 import ini from 'highlight.js/lib/languages/ini';
 import yaml from 'highlight.js/lib/languages/yaml';
+import xml from 'highlight.js/lib/languages/xml';
 import markdown from 'highlight.js/lib/languages/markdown';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import python from 'highlight.js/lib/languages/python';
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('properties', properties);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('html', htmlbars);
 hljs.registerLanguage('ini', ini);
 hljs.registerLanguage('toml', ini);
 hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('xml', xml);
 hljs.registerLanguage('md', markdown);
+hljs.registerLanguage('plaintext', plaintext);
+hljs.registerLanguage('python', python);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code').forEach((block) => {
